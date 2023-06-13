2b7d42f8e162e883b9cb34f71965aaa0ce2dfb52
Shashank <94778190+SlothSpunky77@users.noreply.github.com>
2023-06-13 18:57:45 +0530

    Update pali.c

--- palindrome/pali.c
+++ palindrome/pali.c
@@ -1,4 +1,39 @@
 #include<stdio.h>
+
+//pathetic logic:
+/*
+int power(int b, int e)
+{
+    int p = 1;
+    for (int i = 0; i < e; i++)
+    {
+        p = p*b;
+    }
+    return p;
+}
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("Reverse: ");
+    int c = 0;
+    int num = n;
+    while (n != 0)
+    {
+        int temp = n/10;
+        n = temp;
+        c++;
+    }
+    int sum = 0;
+    for (int i = 1; i <= c; i++)
+    {
+        int temp = num/10;
+        sum = sum + (num - temp*power(10,i)) * power(10,c-i);
+        num = temp;
+    }
+    printf("%d\n",sum);
+    */
 int main()
 {
     int n;
